58e39562ed71e3d283a25686efb2f21f7a00bb16
Juho Vepsalainen <bebraw@gmail.com>
2012-01-04 21:17:29 +0200

    Update caro.js

--- js/jquery.caro.js
+++ js/jquery.caro.js
@@ -1,3 +1,4 @@
+/* MIT (c) Juho Vepsäläinen */
 (function($){
   function caroize($elem, opts) {
     var $slideContainer = $elem.find('.slides');
@@ -7,57 +8,57 @@
     var pos = 0;
     var amount = $slides.length;
     var $navi = $elem.find('.navi');
+    var dir = $elem.find('.up').length? 'top': 'left';
 
     $slideContainer.css('overflow', 'hidden');
     $wrapper.css({
       position: 'relative',
-      width: (amount * 100) + '%'
+      height: (amount * 100) + '%', // vertical case
+      width: (amount * 100) + '%' // horizontal case
     });
 
+    var displayMode = dir == 'top'? 'auto': 'inline-block';
     $slides.each(function(i, e) {
       $(e).css({
-        display: 'inline-block',
-        width: (100 / amount) + '%',
+        display: displayMode,
+        height: (100 / amount) + '%', // vertical case
+        width: (100 / amount) + '%', // horizontal case
         'vertical-align': 'top'
       });
     });
-    
-    function move() {
-      $wrapper.animate({left: (pos * 100) + '%'}, delay);
-    }
-    
-    $elem.find('.left').on('click', function() {
-      pos++;
-      
-      if(pos > 0) {
-        pos = 0;
-      }
-      else {
-        move();
-      }
+
+    $elem.find('.left,.up').on('click', function() {
+      moveTo(pos - 1);
     });
     
-    $elem.find('.right').on('click', function() {
-      pos--;
-      
-      if(pos <= -amount) {
-        pos = -amount + 1;
-      }
-      else {
-        move();
-      }
+    $elem.find('.right,.down').on('click', function() {
+      moveTo(pos + 1);
     });
 
     function moveTo(i) {
-      pos = pos - (i + pos);
-      move();
+      var animProps = {};
+
+      pos = Math.min(Math.max(i, 0), amount - 1);
+      
+      animProps[dir] = (pos * -100) + '%';
+      $wrapper.animate(animProps, delay);
+
+      updateNavi(pos);
+    }
+
+    function updateNavi(i) {
+      var $numberButtons = $navi.find('.number.button');
+      
+      $numberButtons.removeClass('selected');
+      $numberButtons.eq(i).addClass('selected');
     }
     
     $pages = $slides.each(function(i, k) {
       $('<div>').css('display', 'inline').text(i + 1).on('click', function() {
         moveTo(i);
-      }).appendTo($navi).addClass('button');
+      }).appendTo($navi).addClass('number button');
     });
+    updateNavi(0);
   }
   
   $.fn.caro = function (options) {
