d9a02b232ddd24a5d6b1ae45abe6b08e26c2d268
Juho Vepsalainen <bebraw@gmail.com>
2012-01-19 19:01:36 +0200

    Add selected state to member polaroids

--- js/members.js
+++ js/members.js
@@ -1,15 +1,21 @@
 require(['jquery'], function($) {
     $(function() {
-        var firstDescription = $('.polaroid:first .description').html();
+        var $firstPolaroid = $('.polaroid:first');
+        var $firstDescription = $('.description', $firstPolaroid);
+
+        $firstPolaroid.addClass('selected');
 
         // opera hack to make sure descriptions keep hidden (use css instead?)
         $('.polaroid .description').hide();
 
-        $('#info').html(firstDescription);
+        $('#info').html($firstDescription.html());
         var cb = function() {
-            var desc = $('.description', this).html();
+            var $desc = $('.description', this);
 
-            $('#info').html(desc);
+            $('#info').html($desc.html());
+
+            $('.polaroid').removeClass('selected');
+            $(this).addClass('selected');
         };
 
         $('.polaroid').hover(cb).on('click', cb);
