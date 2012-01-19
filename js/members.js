4bed19aa0b516902d5509b5ddaa16f916e42b2ec
Juho Vepsalainen <bebraw@gmail.com>
2012-01-19 18:54:00 +0200

    Hide member descriptions via JS just in case

--- js/members.js
+++ js/members.js
@@ -2,6 +2,9 @@ require(['jquery'], function($) {
     $(function() {
         var firstDescription = $('.polaroid:first .description').html();
 
+        // opera hack to make sure descriptions keep hidden (use css instead?)
+        $('.polaroid .description').hide();
+
         $('#info').html(firstDescription);
         var cb = function() {
             var desc = $('.description', this).html();
