e931b1d67a452060cc3c24765fb3bf1265a9e10e
Juho Vepsalainen <bebraw@gmail.com>
2012-01-21 11:18:57 +0200

    Remove extras from frontpage carousel
    Added "naviClass" option to caro.js too while at it.

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ require(['jquery', 'utils', 'rss', 'twitter', 'jquery.caro'], function($, utils,
         var feeds = ['http://www.kammo.net/feed/', 'http://feeds.feedburner.com/nixtu?format=xml', 'http://blog.deggis.iki.fi/feed/', 'http://feeds.feedburner.com/Esa-mattiSuuronen?format=xml'];
         var twitterUsers = ['bebraw', 'maakuth', 'EsaMatti', 'Zouppen', 'Eladith'];
 
-        $('#projects').caro();
+        $('#projects').caro({naviClass: 'carouselNavi'});
         utils.pick($('#motto'), mottos);
         rss.widget($('#news'), feeds, 5);
         twitter.widget($('#tweets'), twitterUsers, 5);
