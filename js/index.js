c5e09ac7aef198120cf238573cfe1bbd65914b53
Juho Vepsalainen <bebraw@gmail.com>
2012-01-26 17:31:02 +0200

    Make contact form inline

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-require(['jquery', 'utils', 'rss', 'twitter', 'jquery.caro'], function($, utils, rss, twitter) {
+require(['jquery', 'utils', 'rss', 'twitter', 'jquery.caro', 'jquery.yabox'],
+        function($, utils, rss, twitter) {
     $(function() {
         var mottos = ['Conclave of contankerous tinkerers', 'Club of thoughtful tinkerers', 'Happening of happy hipsters', 'Collection of gaudy geeks', 'Motley crew of nice nerds'];
         var feeds = ['http://www.kammo.net/feed/', 'http://feeds.feedburner.com/nixtu?format=xml', 'http://blog.deggis.iki.fi/feed/', 'http://feeds.feedburner.com/Esa-mattiSuuronen?format=xml'];
@@ -8,6 +9,12 @@ require(['jquery', 'utils', 'rss', 'twitter', 'jquery.caro'], function($, utils,
         utils.pick($('#motto'), mottos);
         rss.widget($('#news'), feeds, 5);
         twitter.widget($('#tweets'), twitterUsers, 5);
+
+        $('#contact').yabox({
+            hideOnClick: false,
+            $content: $('#contactForm')
+        });
+        $('#contactForm .close').bind('click', $('#contact').yabox.hide);
 
         /* bg pan test
         $('.hero-unit').on('mousemove', function(e) {
