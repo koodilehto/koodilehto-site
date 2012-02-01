1d70a9d5fe7043ef9e6905c3085c9c27d2d3fe51
Juho Vepsalainen <bebraw@gmail.com>
2012-02-01 21:03:24 +0200

    Attach navigation "selected" class via JS

--- js/ahma.js
+++ js/ahma.js
@@ -1,5 +1,9 @@
 require(['jquery', 'jquery.caro'], function($) {
     $(function() {
         $('#gallery').caro({naviClass: 'carouselNavi'});
+
+        var path = window.location.pathname;
+        $('a[href="' + path + '"]').addClass('selected');
+        $('a[href="/' + path.split('/')[1] + '/"]').addClass('selected');
     });
 });
