f94ac7a483429768d7dca2c7906d38698d14a28b
Juho Vepsalainen <bebraw@gmail.com>
2012-02-12 09:08:28 +0200

    Add Cornix to Twitter users

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ require(['jquery', 'utils', 'rss', 'twitter', 'jquery.caro', 'jquery.yabox'],
     $(function() {
         var mottos = ['Conclave of contankerous tinkerers', 'Club of thoughtful tinkerers', 'Happening of happy hipsters', 'Collection of gaudy geeks', 'Motley crew of nice nerds'];
         var feeds = ['http://www.kammo.net/feed/', 'http://feeds.feedburner.com/nixtu?format=xml', 'http://blog.deggis.iki.fi/feed/', 'http://feeds.feedburner.com/Esa-mattiSuuronen?format=xml'];
-        var twitterUsers = ['bebraw', 'maakuth', 'EsaMatti', 'Zouppen', 'Eladith'];
+        var twitterUsers = ['bebraw', 'maakuth', 'EsaMatti', 'Zouppen', 'Eladith', 'Cornix_'];
 
         $('#projects').caro({naviClass: 'carouselNavi'});
         utils.pick($('#motto'), mottos);
