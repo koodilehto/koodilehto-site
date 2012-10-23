ca569bd357c75a38cc3dcbb9bd00b78367099602
Juho Vepsäläinen <bebraw@gmail.com>
2012-10-23 12:00:57 +0300

    Fix happening typo

--- js/motto.js
+++ js/motto.js
@@ -1,5 +1,5 @@
 define(['utils'], function(utils) {
-    var mottos = ['Conclave of contankerous tinkerers', 'Club of thoughtful tinkerers', 'Happen    ing of happy hipsters', 'Collection of gaudy geeks', 'Motley crew of nice nerds'];
+    var mottos = ['Conclave of contankerous tinkerers', 'Club of thoughtful tinkerers', 'Happening of happy hipsters', 'Collection of gaudy geeks', 'Motley crew of nice nerds'];
 
     return {
         pick: function() {
