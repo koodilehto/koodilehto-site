4fe2c10608705e29e8507edfe778847bd9fc6487
Juho Vepsäläinen <bebraw@gmail.com>
2012-12-11 13:05:53 +0200

    Animate member change

--- js/members.js
+++ js/members.js
@@ -11,21 +11,30 @@ require(['motto', 'jquery', 'jquery.hashchange'], function(motto, $) {
             window.location.hash = '/' + $(this).attr("id");
         });
 
-        $(window).bind('hashchange', function() {
+        $(window).bind('hashchange', function(e, skipAnimation) {
             var personId = window.location.hash.replace('/', '');
             var $polaroid = $(personId).length > 0? $(personId): $('.polaroid:first');
             var $name = $('.name', $polaroid);
             var $desc = $('.description', $polaroid);
 
-            $('#name').text($name.html());
-            $('#info').html($desc.html());
+            if(skipAnimation) {
+                $('#name').text($name.html());
+                $('#info').html($desc.html());
+            }
+            else {
+                $('#name, #info').fadeOut(function() {
+                    $('#name').text($name.html());
+                    $('#info').html($desc.html());
+
+                    $('#name, #info').fadeIn();
+                });
+            }
 
             $('.polaroid').removeClass('selected');
             $polaroid.addClass('selected');
         });
 
         // Force trigger hashchange so that we don't see blank on start
-        $(window).trigger('hashchange');
-
+        $(window).trigger('hashchange', true);
     });
 });
