fe4ad06c6845acbce7b51a49676dd7a421a757c6
Juho Vepsäläinen <bebraw@gmail.com>
2012-12-28 12:15:34 +0200

    Link to blogs of professionals

--- js/rss.js
+++ js/rss.js
@@ -20,30 +20,31 @@ define(['jquery', 'utils'], function($, utils) {
             url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
             dataType: 'json',
             success: function(data) {
-                callback(data.responseData? data.responseData.feed: {author: null, entries: []});
+                callback(data.responseData? data.responseData.feed: {
+                    author: null,
+                    entries: []
+                });
             }
         });
     }
 
+    // TODO: might want to decompose postsPerFeed and publishedDate
+    // thingy out of this
     function orderRSSEntries(data, amount, postsPerFeed) {
         postsPerFeed = postsPerFeed || 1;
-        var entries = [];
 
-        $.each(data, function(i, k) {
-            var author = k.author;
+        return utils.orderEntries($.map(data, function(v) {
+            var author = v.author;
+            var url = v.link;
 
-            entries = entries.concat(k.entries.slice(0, postsPerFeed));
+            return $.map(v.entries.slice(0, postsPerFeed), function(v) {
+                v.author = v.author || author;
+                v.url = url;
+                v.publishedDate = new Date(v.publishedDate);
 
-            $.each(entries, function(i, k) {
-                k.author = k.author || author;
+                return v;
             });
-        });
-
-        $.each(entries, function(i, k) {
-            k.publishedDate = new Date(k.publishedDate);
-        });
-
-        return utils.orderEntries(entries, amount);
+        }), amount);
     }
 
     function constructRSSUI($parent, entries) {
@@ -51,7 +52,7 @@ define(['jquery', 'utils'], function($, utils) {
 
         $.each(entries, function(i, k) {
             $('<dt>').append('<span class="date">' + utils.ISODateString(k.publishedDate) + '</span>').
-                append('<span class="author">' + k.author + '</span>').appendTo($dl);
+                append('<span class="author"><a href="' + k.url + '">' + k.author + '</a></span>').appendTo($dl);
             $('<dd>').attr('title', utils.htmlDecode(k.contentSnippet)).append('<a href="' + k.link + '"><span class="title">' + k.title + '</span></a>').appendTo($dl);
         });
     }
