fae6eb3c44d412de7e2961702c0c1c1a37687239
Juho Vepsalainen <bebraw@gmail.com>
2013-02-16 09:19:13 +0200

    Remove carousel from showcase
    Carousels are so 2012.

--- js/index.js
+++ js/index.js
@@ -1,18 +1,10 @@
-require(['jquery', 'rss', 'twitter', 'jquery.caro', 'jquery.yabox'],
+require(['jquery', 'rss', 'twitter'],
         function($, rss, twitter) {
     $(function() {
         var feeds = ['http://www.kammo.net/feed/', 'http://feeds.feedburner.com/nixtu?format=xml', 'http://blog.deggis.iki.fi/feed/', 'http://feeds.feedburner.com/Esa-mattiSuuronen?format=xml'];
         var twitterUsers = ['bebraw', 'maakuth', 'EsaMatti', 'Zouppen', 'Eladith', 'Cornix_', 'deggis'];
 
-        $('#projects').caro({naviClass: 'carouselNavi', buttonClass: 'btn', cycle: true});
-
         rss.widget($('#news'), feeds, 5);
         twitter.widget($('#tweets'), twitterUsers, 5);
-
-        $('#contact').yabox({
-            hideOnClick: false,
-            $content: $('#contactForm')
-        });
-        $('#contactForm .close').bind('click', $('#contact').yabox.hide);
     });
 });
