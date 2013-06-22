16d865a68f91acff54978c5662cef50671f7c45e
Juho Vepsalainen <bebraw@gmail.com>
2013-06-22 16:52:15 +0300

    Remove Twitter widget
    Given Twitter dropped their support for API v1.0 and now they expect you
    to use OAuth (and preferably their widget) I'm dropping the widget.
    
    The sidebar of the index looks a bit empty now. Perhaps we can show more
    blog entries there or something.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
-require(['jquery', 'rss', 'twitter'],
-        function($, rss, twitter) {
+require(['jquery', 'rss'],
+        function($, rss) {
     $(function() {
         var feeds = ['http://www.kammo.net/feed/', 'http://feeds.feedburner.com/nixtu?format=xml', 'http://blog.deggis.iki.fi/feed/', 'http://feeds.feedburner.com/Esa-mattiSuuronen?format=xml'];
-        var twitterUsers = ['bebraw', 'maakuth', 'EsaMatti', 'Zouppen', 'Eladith', 'Cornix_', 'deggis'];
 
         rss.widget($('#news'), feeds, 5);
-        twitter.widget($('#tweets'), twitterUsers, 5);
     });
 });
