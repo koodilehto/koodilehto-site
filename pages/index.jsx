99ad941e03ed0e7e8ac6dd2f485f53ee6c8675d9
Juho Vepsalainen <bebraw@gmail.com>
2015-06-05 15:11:20 +0300

    Add showcase

--- pages/index.jsx
+++ pages/index.jsx
@@ -36,19 +36,47 @@ var Index = React.createClass({
 
                     <h2>Showcase</h2>
 
-                    <p>We prefer to work using open source technologies. This is visible in our showcase below. In fact the technology this site is built upon is available. Check out <a href='https://antwarjs.github.io/'>Antwar</a>, the static site generator developed on top of Webpack and React.</p>
+                    <p>We prefer to work using open source technologies. This is visible in our showcase below.</p>
 
-                    <h3>Kryptoradio</h3>
+                    <h3><a href='http://kryptoradio.koodilehto.fi/'>Kryptoradio</a></h3>
 
-                    <p>...</p>
+                    <p>
+                        <img className='inline-img' alt="Kryptoradio" src="images/kryptoradio.png" />
+                    </p>
 
-                    <h3>Päätökset.fi - Jyväskylä</h3>
+                    <p>Kryptoradio makes it possible to access the bitcoin network from anywhere, through DVB. It enables new ways to perform monetary transactions with minimal costs. Currently we are running trials on the technology at Finland.</p>
 
-                    <p>...</p>
+                    <h3><a href='http://jkl.paatokset.fi/'>Päätökset.fi - Jyväskylä</a></h3>
 
-                    <h3>Elovalo</h3>
+                    <p>
+                        <img className='inline-img' alt="Kryptoradio" src="images/paatoksetjkl.png" />
+                    </p>
 
-                    <p>...</p>
+                    <p>Päätökset.fi provides access to decisions made at Jyväskylä municipality in various forms. You can for instance check out the map to see which decisions affect you as a resident. Finnish only.</p>
+
+                    <h3><a href='http://elovalo.koodilehto.fi/'>Elovalo</a></h3>
+
+                    <div className="center figure"><img alt="Elovalo led cube in development" src="images/elovalo2.jpg" /><p className="teaser">Elovalo, a led cube and pedestal</p></div>
+
+                    <p>Elovalo was a joint venture between <a href='http://hacklabjkl.org/'>Hacklab Jkl</a>, <a href='http://www.ihmemedia.fi/'>IhmeMedia</a> and Koodilehto. The three cubes produced during the project have effective resolution of 512 voxels and are controllable using C and JavaScript. Our graphics controller is based on AVR ATMega328 and is quite minimal yet effective.</p>
+
+                    <p>The project is a good example of collaboration between local organizations. It showcases our expertise in embedded development and 3D graphics. One instance of this latter was <a href='https://www.blender.org/'>Blender</a> based simulator we implemented and then used to visualize our effects before actual hardware was ready. In addition a JavaScript based IDE was implemented.</p>
+
+                    <h3><a href='https://antwarjs.github.io/'>Antwar</a></h3>
+
+                    <p>
+                        <img className='inline-img' alt="Kryptoradio" src="images/antwar.jpg" />
+                    </p>
+
+                    <p>This site has been built on top of <a href='https://antwarjs.github.io/'>Antwar</a>, a static site generator developed on top of Webpack and React. It is based on <a href='http://braddenver.com/'>Brad Denver</a>'s pioneering work and has been taken much further than the initial concept. <a href='http://eldh.co/'>Andreas Eldh</a> in particular has helped to push it further.</p>
+
+                    <h3><a href='http://survivejs.com/'>SurviveJS - Webpack and React</a></h3>
+
+                    <p>
+                        <img className='inline-img' alt="Kryptoradio" src="images/survivejs.jpg" />
+                    </p>
+
+                    <p>SurviveJS - Webpack and React is an ebook aimed towards developers interested in keeping their frontend fu up to date. These two technologies represent one of the biggest steps forward. The ebook helps you to get ahead of the competition.</p>
                 </div>
             </div>
         );
