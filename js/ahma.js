a5b7fbe38ed31e702574beb355f6eeba8461a944
Juho Vepsalainen <bebraw@gmail.com>
2015-06-06 13:04:14 +0300

    Remove legacy files
    Bye bye Jekyll.

--- js/ahma.js
+++
@@ -1,9 +0,0 @@
-require(['jquery', 'jquery.caro'], function($) {
-    $(function() {
-        $('#gallery').caro({naviClass: 'carouselNavi', buttonClass: 'btn', cycle: true});
-
-        var path = window.location.pathname;
-        $('a[href="' + path + '"]').addClass('selected');
-        $('a[href="/' + path.split('/')[1] + '/"]').addClass('selected');
-    });
-});
