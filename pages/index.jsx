c2cbd5521284ed83088cab6e822c1a11f2d83ca6
Juho Vepsalainen <bebraw@gmail.com>
2015-06-06 13:00:10 +0300

    Tidy up styles

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,35 +3,18 @@ var Members = require('../components/Members.jsx');
 
 module.exports = React.createClass({
     render: function() {
-        var styles = {
-            frontHeader: {
-                fontSize: '54px',
-                lineHeight: '1.5',
-            },
-            headerImage: {
-                background: '#558E8E',
-                backgroundImage: 'url(/images/front.jpg)',
-                backgroundSize: 'cover',
-                backgroundPositionX: '50%',
-            },
-            frontArrow: {
-                paddingTop: '4vw',
-                color: 'rgba(255, 255, 255, 0.4)',
-            },
-        };
-
         return (
             <div className='post post--front'>
-                <div className='header-image header-image--front' style={styles.headerImage}></div>
+                <div className='header-image header-image--front'></div>
                 <div className='post__heading'>
                     <span className='logo logo--front' dangerouslySetInnerHTML={{__html: require('../images/koodilehto_logo_white.svg')}} />
-                    <h1 className='front-header' style={styles.frontHeader}>Koodilehto</h1>
+                    <h1 className='front-header'>Koodilehto</h1>
                     <h3>Combines open technologies for your benefit</h3>
                     <div className='front__buttons'>
                         {/*<a href='blog' className='btn btn--inverted'>Read the Blog</a>*/}
                         <a href='https://github.com/koodilehto' className='btn btn--inverted'>View on GitHub</a>
                     </div>
-                    <div className='front-arrow' style={styles.frontArrow}>&#x25BC;</div>
+                    <div className='front-arrow' />
                 </div>
                 <div className='post__content' dangerouslySetInnerHTML={{__html: require('./index.md').content}} />
                 <div className='post__content'>
